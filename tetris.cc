467533ab9b02bbb81050ce6310e7ae90eadeea3a
Mike Klein <mtklein@gmail.com>
2022-10-21 10:29:35 -0500

    comments

--- tetris.cc
+++ tetris.cc
@@ -1,24 +1,56 @@
 #include <stdint.h>
 #include <stdio.h>
 
-static bool congruent_to_3_mod_4(uint16_t board) {
-    return __builtin_popcount(board) % 4 == 3;
+// 4-row Wordle boards are represented by 15 bits in a uint16_t,
+//
+//    0b abcde'fghij'klmno
+//
+// where abcde are the top row, left to right,
+// fghij the second row and klmno the third row.
+//
+// A green box is represented by a 1 bit, black a 0 bit.
+//
+// The bottom fourth row is assumed to be all green, 11111.
+//
+// So for example, 0b00000'00010'00110 represents this board:
+//
+//      ⬛⬛⬛⬛⬛
+//      ⬛⬛⬛🟩⬛
+//      ⬛⬛🟩🟩⬛
+//      🟩🟩🟩🟩🟩
+
+static bool is_green(uint16_t board, int i) {
+    return board & (1<<i);
 }
 
+// If we win on row 4, we can't have won on earlier rows.
 static bool no_all_green_rows(uint16_t board) {
     return (board & 0b00000'00000'11111) != 0b00000'00000'11111
         && (board & 0b00000'11111'00000) != 0b00000'11111'00000
         && (board & 0b11111'00000'00000) != 0b11111'00000'00000;
 }
 
+// To be tetrizable, the total number of green boxes must divide evenly by 4.
+// There are always 5 green boxes on row 4, so that means the other rows need 3, 7, or 11 greens.
+// (15 could work here too but that's ruled out by no_all_green_rows().)
+static bool congruent_to_3_mod_4(uint16_t board) {
+    return __builtin_popcount(board) % 4 == 3;
+}
+
+// In strict wordle, once a box is green, it stays green on subsequent rows.
 static bool strict(uint16_t board) {
+
+    // No black box on the second row underneath a green box on the top row.
     for (int i = 5; i < 10; i++) {
-        if ( !(board & (1<<i)) && (board & (1<<(i+ 5))) ) { return false; }
+        if (!is_green(board, i) && is_green(board, i+ 5)) { return false; }
     }
+
+    // No black box on the third row underneath a green box on either of the top two rows.
     for (int i = 0; i < 5; i++) {
-        if ( !(board & (1<<i)) && (board & (1<<(i+ 5))) ) { return false; }
-        if ( !(board & (1<<i)) && (board & (1<<(i+10))) ) { return false; }
+        if (!is_green(board, i) && is_green(board, i+ 5)) { return false; }
+        if (!is_green(board, i) && is_green(board, i+10)) { return false; }  // TODO: not needed?
     }
+
     return true;
 }
 
@@ -37,7 +69,8 @@ static void print_board(uint16_t board) {
     printf("🟩🟩🟩🟩🟩\n\n");
 }
 
-int main(int argc, char *argv[]) {
+int main(void) {
+    // Run some simple tests on boards we want maybe_legal() to pass,
     uint16_t const want_legal[] = {
         0b00000'00010'00110,
         0b00111'01111'01111,
@@ -48,7 +81,7 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
-
+    // then more tests on boards we want maybe_legal() to fail.
     uint16_t const want_not_legal[] = {
         0b00000'00000'00000,
         0b11111'11111'11111,
@@ -60,20 +93,15 @@ int main(int argc, char *argv[]) {
         }
     }
 
-    if (argc > 1) {
-        (void)argv;
-
-        int n = 0;
-        for (int bits = 0; bits < (1<<15); bits++) {
-            auto board = static_cast<uint16_t>(bits);
-
-            if (maybe_legal(board)) {
-                print_board(board);
-                n += 1;
-            }
+    // Print and count all maybe-legal boards.
+    int n = 0;
+    for (uint16_t board = 0b00000'00000'00000; board <= 0b11111'11111'11111; board++) {
+        if (maybe_legal(board)) {
+            print_board(board);
+            n += 1;
         }
-        printf("%d maybe-legal boards\n", n);
     }
+    printf("%d maybe-legal boards\n", n);
 
     return 0;
 }
